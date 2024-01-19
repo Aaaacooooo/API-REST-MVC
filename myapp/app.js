22fc2e3ed636a52a7dd1f773bec0569a3497c79b
Aaaacooooo <acoray06@gmail.com>
2024-01-19 21:36:40 +0000

    favicon

--- myapp/app.js
+++ myapp/app.js
@@ -1,3 +1,4 @@
+// Instantiations
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,6 +8,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const favicon = require('serve-favicon'); // Middleware for serving favicon
+var path = require('path')
 var app = express();
 
 // view engine setup
@@ -18,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon(path.join(__dirname, 'public', 'cohete.ico')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
